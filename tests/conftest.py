9dc75a06ef92566afe67f37bbd43cf75b4d6c2a8
Jamie Bliss <astronouth7303@gmail.com>
2016-11-30 21:00:17 -0500

    Refactor tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+import pathlib
+
+from smol.pack.formats import SmolPack
+
+
+@pytest.fixture(scope="session")
+def pack_buffer():
+    fn = pathlib.Path(__file__).parent / "test.smolpack"
+    return fn.read_bytes()
+
+@pytest.fixture()
+def smolpack(pack_buffer):
+    return SmolPack.from_buffer(pack_buffer)
--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,40 +1,27 @@
 import pytest
-import pathlib
+from smol.pack.formats import Version
 
-from smol.pack.formats import SmolPack, Version
-
-
-@pytest.fixture(scope="session")
-def pack_buffer():
-    fn = pathlib.Path(__file__).parent / "test.smolpack"
-    return fn.read_bytes()
-
-
-def test_metadata(pack_buffer):
-    pack = SmolPack.from_buffer(pack_buffer)
-    assert pack['id'] == 'testpack'
-    assert pack['name'] == 'Test Pack'
-    assert pack['description'].strip() == 'baz\nquux'
-    assert pack['version'] == '1.0'
-    assert pack['platforms'] == [('minecraft', '1.10.0')]
-    assert 'files' not in pack
+def test_metadata(smolpack):
+    assert smolpack['id'] == 'testpack'
+    assert smolpack['name'] == 'Test Pack'
+    assert smolpack['description'].strip() == 'baz\nquux'
+    assert smolpack['version'] == '1.0'
+    assert smolpack['platforms'] == [('minecraft', '1.10.0')]
+    assert 'files' not in smolpack
 
 
-def test_metadata_version(pack_buffer):
-    pack = SmolPack.from_buffer(pack_buffer)
-    assert pack['version'].tuple == (1, 0)
+def test_metadata_version(smolpack):
+    assert smolpack['version'].tuple == (1, 0)
 
 
 @pytest.mark.asyncio
-async def test_bundled_file(pack_buffer):
-    pack = SmolPack.from_buffer(pack_buffer)
-    assert (await pack.read_string('foo')).strip() == 'bar'
+async def test_bundled_file(smolpack):
+    assert (await smolpack.read_string('foo')).strip() == 'bar'
 
 
 @pytest.mark.asyncio
-async def test_remote_file(pack_buffer):
-    pack = SmolPack.from_buffer(pack_buffer)
-    assert (await pack.read_string('spam')).strip() == 'eggs'
+async def test_remote_file(smolpack):
+    assert (await smolpack.read_string('spam')).strip() == 'eggs'
 
 
 def test_version_eq():
